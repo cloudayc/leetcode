48648486dc2e5b9f0f7ebd40409da3f95058f94b
cloudayc <cloudayc@gmail.com>
2014-12-30 03:02:56 +0800

    udpate ans

--- LTcode/LTcode/merge-sorted-array.cpp
+++ LTcode/LTcode/merge-sorted-array.cpp
@@ -19,6 +19,30 @@
 class Solution {
 public:
     void merge(int A[], int m, int B[], int n) {
-        
+        int index = m + n - 1;
+        int pa = m - 1;
+        int pb = n - 1;
+        while (index >= 0)
+        {
+            if (pa >= 0 && pb >= 0)
+            {
+                if (A[pa] >= B[pb])
+                {
+                    A[index--] = A[pa--];
+                }
+                else
+                {
+                    A[index--] = B[pb--];
+                }
+            }
+            else if (pa >=0 && pb < 0)
+            {
+                A[index--] = A[pa--];
+            }
+            else if (pb >= 0 && pa < 0)
+            {
+                A[index--] = B[pb--];
+            }
+        }
     }
 };
